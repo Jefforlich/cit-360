c3d002b8b9bcc572952022e7113ed0c2bab5d1cb
Jefforlich <jefforlich@gmail.com>
2016-11-05 19:30:10 -0700

    Update intra.tf

--- terraform/intra.tf
+++ terraform/intra.tf
@@ -8,6 +8,7 @@ provider "aws" {
   region = "us-west-2"
 }
 
+#Creates the default internet gateway
 resource "aws_internet_gateway" "gw" {
   vpc_id = "${var.vpc_id}"
 
@@ -16,6 +17,81 @@ resource "aws_internet_gateway" "gw" {
   }
 }
 
+#resource for the NAT
+resource "aws_eip" "nat" {
+  vpc = true
+}
+
+#The NAT Gateway for the private subnets
+resource "aws_nat_gateway" "ngw" {
+  allocation_id = "${aws_eip.nat.id}"
+  subnet_id = "${aws_subnet.public_subnet_a.id}"
+}
+
+#Creates three public subnets in the us-west-2 region for AWS
+#Public Subnet (a)
+resource "aws_subnet" "public_subnet_a" {
+    vpc_id = "${var.vpc_id}"
+    cidr_block = "172.31.0.0/24"
+    availability_zone = "us-west-2a"
+
+    tags {
+        Name = "public_a"
+    }
+}
+#Public subnet (b)
+resource "aws_subnet" "public_subnet_b" {
+  vpc_id = "${var.vpc_id}"
+  cidr_block = "172.31.1.0/24"
+  availability_zone = "us-west-2b"
+
+  tags {
+    Name = "public_b"
+  }
+}
+#Public subnet (c)
+resource "aws_subnet" "public_subnet_c" {
+  vpc_id = "${var.vpc_id}"
+  cidr_block = "172.31.2.0/24"
+  availability_zone = "us-west-2c"
+
+  tags {
+    Name = "public_c"
+  }
+}
+
+#Creates three PRIVATE subnets for us-west-2 region of AWS
+#Pvt subnet (a)
+resource "aws_subnet" "private_subnet_a"{
+  vpc_id = "${var.vpc_id}"
+  cidr_block = "172.31.4.0/22"
+  availability_zone = "us-west-2a"
+
+  tags {
+    Name = "private_a"
+  }
+}
+#Pvt subnet (b)
+resource "aws_subnet" "private_subnet_b"{
+  vpc_id = "${var.vpc_id}"
+  cidr_block = "172.31.8.0/22"
+  availability_zone = "us-west-2b"
+
+  tags {
+    Name = "private_b"
+  }
+}
+#Pvt subnet (c)
+resource "aws_subnet" "private_subnet_c"{
+  vpc_id = "${var.vpc_id}"
+  cidr_block = "172.31.12.0/22"
+  availability_zone = "us-west-2c"
+
+  tags {
+    Name = "private_c"
+  }
+}
+#Public routing table created
 resource "aws_route_table" "public_routing_table" {
   vpc_id = "${var.vpc_id}"
   route {
@@ -27,18 +103,82 @@ resource "aws_route_table" "public_routing_table" {
     Name = "public_routing_table"
   }
 }
+#Private routing table created for NAT
+resource "aws_route_table" "private_routing_table" {
+  vpc_id = "${var.vpc_id}"
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = "${aws_nat_gateway.ngw.id}"
+  }
 
-resource "aws_subnet" "public_subnet_a" {
-    vpc_id = "${var.vpc_id}"
-    cidr_block = ""
-    availability_zone = "us-west-2a"
-
-    tags {
-        Name = "public_a"
-    }
+  tags {
+    Name = "private_routing_table"
+  }
 }
 
+#Associating each subnet to a routing table
 resource "aws_route_table_association" "public_subnet_a_rt_assoc" {
     subnet_id = "${aws_subnet.public_subnet_a.id}"
     route_table_id = "${aws_route_table.public_routing_table.id}"
+}
+
+resource "aws_route_table_association" "public_subnet_b_rt_assoc" {
+    subnet_id = "${aws_subnet.public_subnet_b.id}"
+    route_table_id = "${aws_route_table.public_routing_table.id}"
+}
+
+resource "aws_route_table_association" "public_subnet_c_rt_assoc" {
+    subnet_id = "${aws_subnet.public_subnet_c.id}"
+    route_table_id = "${aws_route_table.public_routing_table.id}"
+}
+
+resource "aws_route_table_association" "private_subnet_a_rt_assoc" {
+  subnet_id = "${aws_subnet.private_subnet_a.id}"
+  route_table_id = "${aws_route_table.private_routing_table.id}"
+}
+
+resource "aws_route_table_association" "private_subnet_b_rt_assoc" {
+  subnet_id = "${aws_subnet.private_subnet_b.id}"
+  route_table_id = "${aws_route_table.private_routing_table.id}"
+}
+
+resource "aws_route_table_association" "private_subnet_c_rt_assoc" {
+  subnet_id = "${aws_subnet.private_subnet_c.id}"
+  route_table_id = "${aws_route_table.private_routing_table.id}"
+}
+
+#The security group to allow SSH and only allow IP's from a certain
+#CIDR block.
+resource "aws_security_group" "ssh" {
+  name = "cit360_example"
+  vpc_id = "${var.vpc_id}"
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    #The IP block that is allowed to connect
+    cidr_blocks = ["204.152.207.194/24"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+#The instance to be launched on a public subnet
+resource "aws_instance" "cit360_example" {
+  ami = "ami-5ec1673e"
+  instance_type = "t2.micro"
+  subnet_id = "${aws_subnet.public_subnet_a.id}"
+  associate_public_ip_address = true
+  key_name = "cit360"
+  vpc_security_group_ids = ["${aws_security_group.ssh.id}"]
+
+  tags {
+    Name = "CIT360_Example_Instance"
+  }
 }
