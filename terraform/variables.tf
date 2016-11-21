78fbe6ce715a2e970c6cdc0df537ec3188b599dd
Jefforlich <jefforlich@gmail.com>
2016-11-20 21:41:14 -0800

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,4 +8,26 @@ variable "vpc_cidr" {
   description = "VPC CIDR for usage throughout"
   default = "172.31.0.0/16"
 }
-variable "password" {}
+variable "password" {
+  
+}
+
+variable "aws_access_key"{
+
+}
+
+variable "aws_secret_key"{
+
+}
+
+variable "aws_key_path"{
+
+}
+variable "aws_key_name"{
+
+}
+
+variable "username"{
+
+}
+
